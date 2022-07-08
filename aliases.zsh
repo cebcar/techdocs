cae26fc978491f8cb5e8ab43a519a899465c28e0
Carol Clark <3742721+carolclark@users.noreply.github.com>
2022-07-08 15:17:36 -0500

    update 'gsb' display
    line before prompt text allows smaller header
    also update line for working branch

--- aliases.zsh
+++ aliases.zsh
@@ -2,6 +2,6 @@ alias gba='for k in `git branch --all | sed s/^..//` ; do echo -e `git log -1 --
 alias gbn='git branch --no-merged '
 alias gbs="git for-each-ref --sort=committerdate refs/heads/ --format='%(committerdate:short) %(refname:short)'"
 alias gtb='mkdir -p .cebcar; branchlist=".cebcar/branchlist"; git branch | grep -n "[a-z,A-Z]" > $branchlist; git branch | grep -n "[a-z,A-Z]" > $branchlist; cat $branchlist; echo -n "Select: "; read lineno; tb=$(cat $branchlist | grep "$lineno:" | cut -c 5-); echo "tb: $tb"; rm $branchlist'
-alias gsb='autoload -U colors && colors;echo "\n====>>\n=====>>\n";git status;echo "\nall branches:";git branch; echo "temp: $fg[cyan]$tb$reset_color\n"; echo "unmerged branches:\n";git branch --no-merged'
+alias gsb='autoload -U colors && colors;echo "$fg[yellow]- git status and branches -$reset_color\n";git status;echo "\nall branches:";git branch;echo "    wb: $fg[cyan]$wb$reset_color\n";echo "unmerged branches:\n";git branch --no-merged'
 alias glgr='git log --oneline --graph'
 alias glog='git log --oneline --graph -6'
